79b40ac655ba2835a7aeab0464e4f7a4e4757756
dcstewart <dcstewart@TNB5CD9484N98.mshome.net>
2023-04-17 13:41:27 -0400

    Provided Template

--- src/student/Main.java
+++ src/student/Main.java
@@ -4,17 +4,7 @@ import framework.SimpleIO;
 public class Main {
        
     public Main() {
-        SlotMachine machine = new SlotMachine();
-        
-        double money = 50;
-        for (int i = 0; i < 30; i++) {
-            money -= 0.25;
-            
-            double payout = machine.play();
-            money += payout;
-            io.write("Payout: "+payout+", Balance: "+money);
-        }
-        
+
     }
     
     private final SimpleIO io = new SimpleIO();
--- src/student/SlotMachine.java
+++ src/student/SlotMachine.java
@@ -4,24 +4,8 @@ import java.util.Random;
 
 public class SlotMachine {
     
-    private double amount;
-    
-    public SlotMachine() {
-        this.amount = 100;
-    }
-    
-    public double play() {
-        Random random = new Random();
-        int num = random.nextInt(100);
-        amount += 0.25;
-        if (num < 95) 
-            return 0;
-        double payout = 100-num;
-        if (payout > amount)
-            payout = amount;
-        amount -= payout; 
-        return payout;
-    }
-      
+
+
+ 
     private final SimpleIO io = new SimpleIO();
 }
